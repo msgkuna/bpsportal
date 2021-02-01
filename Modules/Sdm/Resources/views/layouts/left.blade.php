8f0515666114b5f3d9a38e4f614cd491fd3ebd09
Muhammad Said <msgkuna@gmail.com>
2021-02-01 19:12:03 +0700

    update

--- Modules/Sdm/Resources/views/layouts/left.blade.php
+++ Modules/Sdm/Resources/views/layouts/left.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
-        <img src="{{ asset('/images/logo.png')}}" alt="BPS Logo" class="brand-image  elevation-3">
+        <img src="{{ asset('/images/logo.png')}}" alt="BPS Logo" class="brand-image">
         <span class="logo-lg">{{ config('app.name', 'Laravel') }}</span>
     </a>
 
